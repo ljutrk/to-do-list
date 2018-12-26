f9e6ba067f19080c4a077f9d808cdba948861ded
Ljubomir Trkulja <l.trkulja@msgnetconomy.net>
2018-12-26 16:26:22 +0100

    added basic layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import TaskList from './TaskList';
 
 const App = () => {
     return (
         <div className="app">
-            To Do List!
+            <h1 className="app-heading">To Do List!</h1>
+            <div className="buttons">
+                <button className="buttons-to-do">To Do</button>
+                <button className="buttons-done">Done</button>
+                <button className="buttons-all">All</button>
+            </div>
+            <TaskList />
         </div>
     );
 }
---
+++ src/components/TaskList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TaskList = () => {
+    return (
+        <div className="tasklist">
+            Tasklist
+        </div>
+    );
+}
+
+export default TaskList;

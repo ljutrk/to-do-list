2c475ea6ed71110a6bb31408ef646fb18998ca28
Ljubomir Trkulja <l.trkulja@msgnetconomy.net>
2018-12-26 18:06:49 +0100

    added redux and react-redux to project, added click handlers for buttons

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,13 @@ const App = () => {
         <div className="app">
             <h1 className="app-heading">To Do List!</h1>
             <div className="buttons">
-                <button className="buttons-to-do">To Do</button>
-                <button className="buttons-done">Done</button>
-                <button className="buttons-all">All</button>
+                <button className="buttons-to-do" onClick={() => console.log("show to-do task list")}>To Do</button>
+                <button className="buttons-done" onClick={() => console.log("show finished task list")}>Done</button>
+                <button className="buttons-all" onClick={() => console.log("show all tasks")}>All</button>
             </div>
             <div className="task-new">
                 <input type="text" />
-                <button><i className="fas fa-plus"></i></button>
+                <button onClick={() => console.log("add new task to list")}><i className="fas fa-plus"></i></button>
             </div>
             <TaskList />
         </div>
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Task = () => {
     return (
         <div className="task">
-            <input type="checkbox" name="task-status" id="task-status" />
+            <input type="checkbox" name="task-status" id="task-status" onClick={() => console.log("finished task button")} />
             <div className="task-content-wrapper">
                 <p>Go buy some apples!</p>
                 <div className="icons-wrapper">
-                    <i className="fas fa-edit"></i>
-                    <i className="fas fa-trash-alt"></i>
+                    <i className="fas fa-edit" onClick={() => console.log("edit button")}></i>
+                    <i className="fas fa-trash-alt" onClick={() => console.log("delete button")}></i>
                 </div>
             </div>
         </div>

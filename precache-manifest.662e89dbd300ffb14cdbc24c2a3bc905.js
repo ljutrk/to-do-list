52783d5565569bc24f8fe0c2e9e85c776716fdfd
Ljubomir Trkulja <l.trkulja@msgnetconomy.net>
2018-12-27 13:59:11 +0100

    Updates

---
+++ precache-manifest.662e89dbd300ffb14cdbc24c2a3bc905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af0bf1497ad58103b57b",
+    "url": "/to-do-list/static/js/runtime~main.af0bf149.js"
+  },
+  {
+    "revision": "150c9916d898263e8571",
+    "url": "/to-do-list/static/js/main.150c9916.chunk.js"
+  },
+  {
+    "revision": "c838ad3926d4cbaf9b6a",
+    "url": "/to-do-list/static/js/1.c838ad39.chunk.js"
+  },
+  {
+    "revision": "150c9916d898263e8571",
+    "url": "/to-do-list/static/css/main.8e529d65.chunk.css"
+  },
+  {
+    "revision": "de10b77c6d238caef3ac4d3ca64d2fd0",
+    "url": "/to-do-list/index.html"
+  }
+];
